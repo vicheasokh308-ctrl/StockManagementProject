58ca58061706067cb4d893c9c0f5a594ecb2fb6b
visaldara496-lang <visaldara496@gmail.com>
2026-01-22 22:08:04 +0700

    New file2

--- StockManagementProject/obj/Debug/net9.0/StockManagementProject.AssemblyInfo.cs
+++ StockManagementProject/obj/Debug/net9.0/StockManagementProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockManagementProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d83f963722567041637afda8b03ef8987a77c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfaaf52dcb244031d4e4b522a0a09feb07bc17b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockManagementProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockManagementProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
